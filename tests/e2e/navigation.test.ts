d23ce0d86e1341fa2aa779af4049571458d6559c
Gleb Elenev <elenev@greensight.ru>
2025-08-17 21:22:47 +0300

    Revert "del"
    This reverts commit dd02dbcfe2cb2642009e001ff8a55571231ad6d3.

---
+++ tests/e2e/navigation.test.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "@playwright/test";
+import { DashboardPage } from "@pages/DashboardPage";
+import { UserPage } from "@pages/UserPage";
+import { StatusesListPage } from "@pages/StatusesListPage";
+import { Menu } from "tests/enums";
+import { LabelsListPage } from "@pages/LabelsListPage.ts";
+
+test.describe("navigation", () => {
+  test("should navigate to Users page", async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const usersPage = new UserPage(page);
+
+    await dashboardPage.goto();
+    await dashboardPage.waitForLoad();
+    await dashboardPage.sidebar.clickMenu(Menu.USERS);
+
+    await expect(usersPage.heading).toBeVisible();
+  });
+  test("should navigate to Statuses page", async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const statusesPage = new StatusesListPage(page);
+
+    await dashboardPage.goto();
+    await dashboardPage.waitForLoad();
+    await dashboardPage.sidebar.clickMenu(Menu.STATUSES);
+
+    await expect(statusesPage.heading).toBeVisible();
+  });
+
+  test("should navigate to Labels page", async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const labelsPage = new LabelsListPage(page);
+
+    await dashboardPage.goto();
+    await dashboardPage.waitForLoad();
+    await dashboardPage.sidebar.clickMenu(Menu.LABELS);
+
+    await expect(labelsPage.heading).toBeVisible();
+  });
+});
